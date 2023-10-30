459504258d2879fae4ea518ae0e03d7969f4be86
Alexon Uthayakumar <authayakumar@reseau.free.fr>
2023-10-30 15:12:50 +0100

    add pre-commit flake8 linter

--- authentication/views.py
+++ authentication/views.py
@@ -26,6 +26,8 @@ class CurrentUserView(APIView):
     #         if serializer.is_valid():
     #             serializer.save()
     #             return Response(serializer.data)
-    #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    #         return Response(serializer.errors,
+    #                         status=status.HTTP_400_BAD_REQUEST
+    #               )
     #     except User.DoesNotExist:
     #         return Response(status=status.HTTP_404_NOT_FOUND)
