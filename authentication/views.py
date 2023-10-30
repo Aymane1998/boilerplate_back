c4d4bc40a37b9226731b8c1fb01d1473698e7fcb
Alexon Uthayakumar <authayakumar@reseau.free.fr>
2023-10-30 16:48:14 +0100

    add doc

--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,11 @@
 """This module handles authentication views."""
 
+from rest_framework import permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import permissions
+
 from rest_framework_simplejwt.views import TokenObtainPairView
+
 from .serializers import MyTokenObtainPairSerializer, UserSerializer
 
 # Create your views here.
